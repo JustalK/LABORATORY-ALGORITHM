3ca61d038fea8fe0b654ed8a1e8a502bfbae8fc0
justalk <justal.kevin@gmail.com>
2022-12-17 14:55:15 +0100

    [FEATURE] Creating first part of the algo

--- Astar/index.js
+++ Astar/index.js
@@ -47,6 +47,80 @@ class Graph {
     }
   }
 
+  findPath(xStart, yStart, xEnd, yEnd) {
+    const openTiles = [];
+    const closedTiles = [];
+    let currentTile;
+
+    const startTile = this.getTileAt(xStart, yStart);
+    openTiles.add(startTile);
+
+    while (openTiles.length !== 0) {
+      currentTile = openTiles.reduce((best, current) =>
+        best && current.f > best.f ? best : current
+      );
+
+      // Add the best tile to closed list
+      closedTiles.add(currentTile);
+
+      // Remove the currentTile to open tiles
+      const indexCurrentTile = openTiles.indexOf(
+        (t) => t.x === currentTile.x && t.y === currentTile.y
+      );
+      openTiles.splice(indexCurrentTile, 1);
+
+      // Check
+      const isEnd = closedTiles.some((t) => t.x === xEnd && t.y === yEnd);
+      if (isEnd) {
+        // PATH FOUND
+        break;
+      }
+
+      const adjacentTiles = this.getAdjacentTiles(currentTile.x, currentTile.y);
+
+      for (const adjacentTile of adjacentTiles) {
+        const isAlreadyClosedTile = closedTiles.some(
+          (t) => t.x === adjacentTile.x && t.y === adjacentTile.y
+        );
+        if (isAlreadyClosedTile) {
+          continue;
+        }
+
+        const isAlreadyOpenTile = openTiles.some(
+          (t) => t.x === adjacentTile.x && t.y === adjacentTile.y
+        );
+        if (!isAlreadyOpenTile) {
+          openTiles.add(adjacentTile);
+        } else {
+          // Dont get it
+        }
+      }
+    }
+  }
+
+  getTileAt(x, y) {
+    return this.map.find((t) => t.x === x && t.y === y);
+  }
+
+  getAdjacentTiles(x, y) {
+    const rsl = [];
+    const adjacentPositions = [
+      { x: x + 1, y },
+      { x: x - 1, y },
+      { x, y: y + 1 },
+      { x, y: y - 1 },
+    ];
+
+    for (const adjacentPosition of adjacentPositions) {
+      const tile = this.getTileAt(adjacentPosition.x, adjacentPosition.y);
+      if (tile) {
+        rsl.push(tile);
+      }
+    }
+
+    return rsl;
+  }
+
   toString() {
     let i = 0;
     let rsl = [];

037c876b71933a4bf4afba36571c146b1c2dab66
justalk <justal.kevin@gmail.com>
2022-12-20 21:35:07 +0100

    [FEATURE] Finding the DAG for my codingame

---
+++ DAG/index.js
@@ -0,0 +1,33 @@
+class Tile {
+  left;
+  top;
+  right;
+  bottom;
+
+  update(left = null, top = null, right = null, bottom = null) {
+    this.left = left;
+    this.top = top;
+    this.right = right;
+    this.bottom = bottom;
+  }
+}
+
+class Graph {
+  constructor() {
+    const A = new Tile(),
+      B = new Tile(),
+      C = new Tile(),
+      D = new Tile(),
+      E = new Tile();
+
+    A.update(null, null, B, null);
+    B.update(A, null, null, C);
+    C.update(null, B, D, E);
+    D.update(C, null, null, null);
+    E.update(null, C, null, null);
+  }
+}
+
+// A B X X
+// X C D X
+// X E X X
